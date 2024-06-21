2fd146e8cc0ba3aea6a1f2a0d8cec6f43cf98eed
ClaudioScordari <scordari22@gmail.com>
2024-06-21 18:45:04 +0200

    Completata sezione 2 + responsive

--- src/components/Section_2.vue
+++ src/components/Section_2.vue
@@ -22,17 +22,176 @@
 </script>
 
 <template>
-    <section id="section-2" class="border border-1 border-dark">
+    <section id="section-2">
         <h2 class="fs-1 fw-bold text-center">Cosa ne pensano i nostri sviluppatori</h2>
 
-        <!-- swiper -->
-        <swiper id="swiper" :slidesPerView="1" :spaceBetween="30" :keyboard="{ enabled: true,}" :pagination="{ clickable: true}" :navigation="true" :modules="modules">
+        <!-- swiper-lg -->
+        <swiper id="swiper-lg" :slidesPerView="1" :spaceBetween="30" :keyboard="{ enabled: true,}" :pagination="{ clickable: true}" :navigation="true" :modules="modules">
             <swiper-slide>
-                <!-- slide 1 -->
+                <div class="row">
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">Emma Turner - April 5, 2023</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">Rachel Bryan - April 18, 2023</h5>
+                        </div>
+                    </div>
+                </div>
             </swiper-slide>
 
             <swiper-slide>
-                <!-- slide 2 -->
+                <div class="row">
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">Emma Turner - April 5, 2023</h5>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="my-card">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                            <div>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                                <i class="fa-solid fa-star"></i>
+                            </div>
+
+                            <h5 class="fw-bold mt-4">Rachel Bryan - April 18, 2023</h5>
+                        </div>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
+
+        <!-- swiper-sm -->
+        <swiper id="swiper-sm" :slidesPerView="1" :spaceBetween="0" :keyboard="{ enabled: true,}" :pagination="{ clickable: true}" :navigation="true" :modules="modules">
+            <swiper-slide>
+                <div class="my-card">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                    <div>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+
+                    <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                </div>
+            </swiper-slide>
+
+            <swiper-slide>
+                <div class="my-card">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                    <div>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+
+                    <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                </div>
+            </swiper-slide>
+
+            <swiper-slide>
+                <div class="my-card">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                    <div>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+
+                    <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                </div>
+            </swiper-slide>
+
+            <swiper-slide>
+                <div class="my-card">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt laborum autem quod explicabo nulla tenetur fuga alias quisquam pariatur temporibus, assumenda necessitatibus aliquid corrupti architecto sint velit illo et amet fugit inventore beatae neque dolorum, numquam sequi. Aliquid, vitae asperiores.</p>
+
+                    <div>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                        <i class="fa-solid fa-star"></i>
+                    </div>
+
+                    <h5 class="fw-bold mt-4">David Wiliam - Feb 22, 2023</h5>
+                </div>
             </swiper-slide>
         </swiper>
     </section>
@@ -44,7 +203,40 @@
     @use '../assets/scss/partials/mixins.scss' as *;
 
     // GENERAL
+    #swiper-lg { // di default
+        display: block;
+    }
+
+    #swiper-sm { // di default
+        display: none;
+    }
+
+    #section-2 h2 {
+        margin: 70px 0;
+    }
+
     h2 {
         color: $black-color;
     }
+
+    .my-card {
+        text-align: center;
+        padding: 30px;
+    }
+
+    .fa-star {
+        color: yellow;
+    }
+
+    // mediaquery 
+    @media (max-width: 767px) {
+        #swiper-lg {
+            display: none;
+        }
+
+        #swiper-sm {
+            display: block;
+        }
+    }
+
 </style>
